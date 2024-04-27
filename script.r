cea594756f5f78d9931aec26513212a6e9f51c69
sallyca530 <sally.ca530@gmail.com>
2024-04-26 18:19:37 -0700

    script

--- script.r
+++ script.r
@@ -13,5 +13,20 @@ mtcars
 
 # group the data by gear and find mean wt_lbs as avg_wt
 group_by_gear <- group_by(mtcars, gear)
-mean_wt_lbs_by_gear <- summarize(grouped_by_gear, avg_wt = mean(wt_lbs))
+group_by_gear
+
+mean_wt_lbs_by_gear <- summarize(group_by_gear, avg_wt = mean(wt_lbs))
 mean_wt_lbs_by_gear
+
+# Find the average hp of cars over 3000 pounds
+mt_cars_over_3000 <- subset(mtcars, wt_lbs > 3000)
+mean(mt_cars_over_3000$hp)
+
+# Find the value of carb with the highest avg mpg
+mtcars %>%
+  group_by(vs) %>%
+  summarize(mean_mpg = mean(mpg)) %>%
+  subset(mean_mpg == max(mean_mpg))
+
+
+# Find the mean mpg per value in the vs column
